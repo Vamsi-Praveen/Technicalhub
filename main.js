1dc3f136a50c216164f0b94a006e3d2d4f390c5a
Vamsi-Praveen <21A91A05G5@aec.edu.in>
2023-07-27 15:42:47 +0530

    commit

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 const header = document.getElementById('header');
 const scrl = document.getElementById('scrl');
+const links = document.getElementById('ll');
+const drop = document.getElementById('drop');
+
+links.addEventListener('mousemove',()=>{
+    drop.classList.add('active');
+});
+links.addEventListener('mouseleave',()=>{
+    drop.classList.remove('active');
+})
 window.addEventListener('scroll',()=>{
     header.classList.toggle('active',window.scrollY>80);
     scrl.classList.toggle('active',window.scrollY>80);
